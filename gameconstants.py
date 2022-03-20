53354362939ad06e497ac83d3687ff20bb7d23ca
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-03-20 10:18:32 -0300

    Create gameconstants

---
+++ gameconstants.py
@@ -0,0 +1,12 @@
+import pygameconstants as pgc
+# Map/Grid Constants
+MAP_HEIGHT = 9
+MAP_WIDTH = 16
+
+# Units Stats
+BASE_HEALTH = 1.0
+BASE_DMG = 0.2
+BASE_SPEED = 1/pgc.FREQUENCY # 1 square/s
+BASE_ARMOR = 1.0
+BASE_ARMOR_PIERC = 1.0
+AP_EFFECTIVENESS = 2.0
