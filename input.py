c8157e956952642d484096abdb3ccc83d07b0e30
MateusCristoMelo <mateuscristomelo@hotmail.com>
2022-04-08 01:52:32 -0300

    Add more spawn keybinds

--- input.py
+++ input.py
@@ -92,7 +92,11 @@ class Input:
                     self.game.running = False
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        self.game.spawn_troop()
+                        self.game.spawn_infantry()
+                    elif event.key == pygame.K_v:
+                        self.game.spawn_tank()
+                    elif event.key == pygame.K_b:
+                        self.game.spawn_airforce()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     for button in self.buttons[enums.GameState.PLAYING]:
                         button.press(pygame.mouse.get_pos())
